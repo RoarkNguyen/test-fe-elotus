964fdf0a4d20e3ed41ea10b12d7d550a4cea06c8
Roark Nguyen <76858502+RoarkNguyen@users.noreply.github.com>
2024-04-23 23:07:50 +0700

    change size image

--- src/config/config.ts
+++ src/config/config.ts
@@ -7,7 +7,7 @@ const TOP_RATED_BASE_URL = `${API_URL}movie/top_rated?api_key=${API_KEY}`;
 const NOW_PLAYING_BASE_URL = `${API_URL}movie/now_playing?api_key=${API_KEY}`;
 
 const IMAGE_BASE_URL = 'https://image.tmdb.org/t/p/';
-const CARD_SIZE = 'w200';
+const CARD_SIZE = 'w300';
 const POSTER_SIZE = 'w500';
 
 export { API_URL, API_KEY, SEARCH_BASE_URL, POPULAR_BASE_URL, TOP_RATED_BASE_URL, NOW_PLAYING_BASE_URL, IMAGE_BASE_URL, POSTER_SIZE, CARD_SIZE};
